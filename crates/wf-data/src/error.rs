a586b20be355bfda571e7b406a4e2026dc91704f
Mettwasser <kaicrafter2000@gmail.com>
2026-09-24 21:17:06 +0200

    chore: remove unused error variant

--- crates/wf-data/src/error.rs
+++ crates/wf-data/src/error.rs
@@ -1,11 +1,5 @@
 pub type Result<T> = std::result::Result<T, DataError>;
 
-#[derive(thiserror::Error, Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MappingErrorKind {
-    #[error("not found")]
-    NotFound,
-}
-
 #[derive(Debug, thiserror::Error)]
 pub enum DataError {
     #[error("{0} json: {1}")]
@@ -26,9 +20,4 @@ pub enum DataError {
     #[cfg(feature = "fetch")]
     #[error("No cached copy of {0} and the download failed")]
     NoCacheAvailable(String),
-    #[error("failed to map data for {item_unique_name}: {kind}")]
-    Mapping {
-        item_unique_name: String,
-        kind: MappingErrorKind,
-    },
 }
